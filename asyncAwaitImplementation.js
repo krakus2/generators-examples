e08b45c8bcbdf7d2a44f1282241efed5e8ed43dd
Filip Krakowiak <filip.krakowiak@ynd.co>
2021-02-16 12:06:48 +0100

    Added async-await generator implementation  examples

--- asyncAwaitImplementation.js
+++ asyncAwaitImplementation.js
@@ -1,3 +1,68 @@
-export default function () {
-  console.log('asyncAwait')
+export default async function () {
+  //async-await version
+  try {
+    const result = await fetch(`https://jsonplaceholder.typicode.com/todos`)
+    const todos = await result.json()
+    console.log('async await result', todos)
+  } catch (err) {
+    console.error(err)
+  }
+
+  // generator version - simple implementation
+  // runner is like async keyword
+  function runner(generatorFunction) {
+    const iterator = generatorFunction()
+
+    function run(value) {
+      const result = iterator.next(value)
+      if (result.done) {
+        return result.value
+      } else {
+        //Promise.resolve(value) creates a resolved promise with the result value.
+        //Same as:
+        //const promise = new Promise(resolve => resolve(value));
+        return Promise.resolve(result.value).then(run)
+      }
+    }
+
+    return run()
+  }
+
+  runner(function* () {
+    try {
+      const response = yield fetch(`https://jsonplaceholder.typicode.com/todos`)
+      const todos = yield response.json()
+      console.log('simple generator implementation result', todos)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
+  // generator version - enhanced version with the error handling
+  function runner2(generatorFunction) {
+    const iterator = generatorFunction()
+
+    function run(value) {
+      const result = iterator.next(value)
+      if (result.done) {
+        return result.value
+      } else {
+        return Promise.resolve(result.value).then(run, (err) =>
+          Promise.resolve(iterator.throw(err)).then(run)
+        )
+      }
+    }
+
+    return run()
+  }
+
+  runner2(function* () {
+    try {
+      const response = yield fetch(`https://jsonplaceholder.typicode.com/todos`)
+      const todos = yield response.json()
+      console.log('enhanced generator implementation result', todos)
+    } catch (err) {
+      console.error(err)
+    }
+  })
 }
